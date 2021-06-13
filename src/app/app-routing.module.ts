bbcc6909cceb208b009c36bcf783a61771b8fd3e
SagarPanchal64 <68366079+SagarPanchal64@users.noreply.github.com>
2021-06-13 16:05:43 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BootstrapFormComponent } from './components/bootstrap-form/bootstrap-form.component';
+import { BootstrapReactiveFormComponent } from './components/bootstrap-reactive-form/bootstrap-reactive-form.component';
+import { CompFormComponent } from './components/comp-form/comp-form.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+
+const routes: Routes = [
+  {path: 'form', component: CompFormComponent},
+  {path: 'contactForm', component: ContactFormComponent},
+  {path: 'bootstrap-form', component: BootstrapFormComponent},
+  {path: 'bootstrap-rform', component: BootstrapReactiveFormComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
