31cda5581362079f3daad3d8a0d46f2afda02c25
darkwarrior303 <yashsathwara404@gmail.com>
2021-06-13 18:17:45 +0530

    Login-form component created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { BootstrapFormComponent } from './components/bootstrap-form/bootstrap-fo
 import { BootstrapReactiveFormComponent } from './components/bootstrap-reactive-form/bootstrap-reactive-form.component';
 import { CompFormComponent } from './components/comp-form/comp-form.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 const routes: Routes = [
   {path: 'form', component: CompFormComponent},
   {path: 'contactForm', component: ContactFormComponent},
   {path: 'bootstrap-form', component: BootstrapFormComponent},
-  {path: 'bootstrap-rform', component: BootstrapReactiveFormComponent}
+  {path: 'bootstrap-rform', component: BootstrapReactiveFormComponent},
+  {path: 'login-form', component: LoginFormComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { CompFormComponent } from './components/comp-form/comp-form.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
 import { BootstrapFormComponent } from './components/bootstrap-form/bootstrap-form.component';
 import { BootstrapReactiveFormComponent } from './components/bootstrap-reactive-form/bootstrap-reactive-form.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { BootstrapReactiveFormComponent } from './components/bootstrap-reactive-
     ContactFormComponent,
     BootstrapFormComponent,
     BootstrapReactiveFormComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
